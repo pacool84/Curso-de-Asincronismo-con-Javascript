3e95fc2e266dbfa408a0b6a358befcc64ed232df
Pacool84 <flopez_1384@hotmail.com>
2024-05-19 20:11:01 -0600

    [ADD] Complementando el fetch del archivo challenge

--- src/callbacks/challenge.js
+++ src/callbacks/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require("xmlhttprequest");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API = "https://api.escuelajs.co/api/v1";
 
 function fectData(urlApi, callback) {
@@ -10,11 +10,27 @@ function fectData(urlApi, callback) {
     if (xhttp.readyState === 4) {
       if (xhttp.status === 200) {
         callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error("Error" + urlApi);
+        return callback(error, null);
       }
-    } else {
-      const error = new Error("Error" + urlApi);
-      return callback(error, null);
     }
   };
   xhttp.send();
 }
+fectData(`${API}/products`, function (error1, data1) {
+  if (error1) return console.error(error1);
+
+  fectData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+    if (error2) return console.error(error2);
+    fectData(
+      `${API}/categories/${data2?.category?.id}`,
+      function (error3, data3) {
+        if (error3) return console.error(error3);
+        console.log(data1[0]);
+        console.log(data2.title);
+        console.log(data3.name);
+      }
+    );
+  });
+});
