5bfdee7f1fdac76b87d914e28a47ef99349bcc0b
Pacool84 <flopez_1384@hotmail.com>
2024-05-19 20:17:29 -0600

    [FIX] Typo en el status de onreadystatechange

--- src/callbacks/challenge.js
+++ src/callbacks/challenge.js
@@ -1,12 +1,11 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API = "https://api.escuelajs.co/api/v1";
 
-function fectData(urlApi, callback) {
+function fetchData(urlApi, callback) {
   let xhttp = new XMLHttpRequest();
 
   xhttp.open("GET", urlApi, true);
-
-  xhttp.onredaystatechange = function (event) {
+  xhttp.onreadystatechange = function (event) {
     if (xhttp.readyState === 4) {
       if (xhttp.status === 200) {
         callback(null, JSON.parse(xhttp.responseText));
@@ -18,12 +17,12 @@ function fectData(urlApi, callback) {
   };
   xhttp.send();
 }
-fectData(`${API}/products`, function (error1, data1) {
-  if (error1) return console.error(error1);
 
-  fectData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+fetchData(`${API}/products`, function (error1, data1) {
+  if (error1) return console.error(error1);
+  fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
     if (error2) return console.error(error2);
-    fectData(
+    fetchData(
       `${API}/categories/${data2?.category?.id}`,
       function (error3, data3) {
         if (error3) return console.error(error3);
